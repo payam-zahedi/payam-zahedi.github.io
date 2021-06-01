8c3226af7fa0a9e00f3f89200975bdaaf28a11fe
Payam Zahedi <pzahedi1995@gmail.com>
2021-06-01 12:12:36 +0430

    main.dart refactored

--- lib/main.dart
+++ lib/main.dart
@@ -6,26 +6,29 @@ import 'package:portfolio/resource/index.dart';
 import 'generated/l10n.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(Portfolio());
 }
-
-class MyApp extends StatelessWidget {
+//ToDo(payam) : add What I do section
+//ToDo(payam) : add project section
+//ToDo(payam) : add skills section
+//ToDo(payam) : add code lab
+//ToDo(payam) : add fun fact
+//ToDo(payam) : support multy theme
+class Portfolio extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Payam Zahedi Portfolio',
       theme: portfolioTheme,
-      home: HomeScreen(),
       localizationsDelegates: [
-        // 1
         S.delegate,
-        // 2
         GlobalMaterialLocalizations.delegate,
         GlobalWidgetsLocalizations.delegate,
         GlobalCupertinoLocalizations.delegate,
       ],
       supportedLocales: S.delegate.supportedLocales,
+      home: HomeScreen(),
     );
   }
 }
