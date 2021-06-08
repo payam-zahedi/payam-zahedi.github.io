53333913bbe1673acce98ca6335e32f6a02a173d
Payam Zahedi <pzahedi1995@gmail.com>
2021-06-08 11:49:37 +0430

    fvm config added to project

--- lib/presentation/screens/home/home.dart
+++ lib/presentation/screens/home/home.dart
@@ -9,115 +9,112 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Scrollbar(
-        isAlwaysShown: true,
-        child: Padding(
-          padding: EdgeInsets.symmetric(
-            vertical: 16,
-            horizontal: (context.isMobile()
-                ? 16
-                : context.isTablet()
-                    ? 24
-                    : 64),
-          ),
-          child: CustomScrollView(
-            slivers: [
-              SliverToBoxAdapter(
-                child: AboutMe(),
-              ),
-              SliverPadding(
-                padding: EdgeInsets.only(top: 32),
-                sliver: SliverToBoxAdapter(
-                  child: Center(
-                    child: Text(
-                      context.localization.socialTitle,
-                      style: Theme.of(context).textTheme.subtitle1?.copyWith(
-                            fontSize: 16,
-                            color: Theme.of(context).colorScheme.onSurface,
-                            fontWeight: FontWeight.bold,
-                          ),
-                    ),
+      body: Padding(
+        padding: EdgeInsets.symmetric(
+          vertical: 16,
+          horizontal: (context.isMobile()
+              ? 16
+              : context.isTablet()
+                  ? 24
+                  : 64),
+        ),
+        child: CustomScrollView(
+          slivers: [
+            SliverToBoxAdapter(
+              child: AboutMe(),
+            ),
+            SliverPadding(
+              padding: EdgeInsets.only(top: 32),
+              sliver: SliverToBoxAdapter(
+                child: Center(
+                  child: Text(
+                    context.localization.socialTitle,
+                    style: Theme.of(context).textTheme.subtitle1?.copyWith(
+                          fontSize: 16,
+                          color: Theme.of(context).colorScheme.onSurface,
+                          fontWeight: FontWeight.bold,
+                        ),
                   ),
                 ),
               ),
-              SliverPadding(
-                padding: EdgeInsets.only(top: 16),
-                sliver: SliverToBoxAdapter(
-                  child: Center(child: SocialLinksWidget()),
-                ),
+            ),
+            SliverPadding(
+              padding: EdgeInsets.only(top: 16),
+              sliver: SliverToBoxAdapter(
+                child: Center(child: SocialLinksWidget()),
               ),
-              SliverToBoxAdapter(
-                child: TextHint(text: 'What I Do'),
-              ),
-              SliverToBoxAdapter(
-                child: WhatIDoWidget(),
-              ),
-              // SliverToBoxAdapter(child: TextHint(text: 'Education')),
-              // SliverStaggeredGrid.extent(
-              //   maxCrossAxisExtent: 600,
-              //   staggeredTiles: [
-              //     StaggeredTile.fit(1),
-              //     StaggeredTile.fit(1),
-              //     StaggeredTile.fit(1),
-              //   ],
-              //   children: [
-              //     TimeLineTile(
-              //       position: 'Graphic Design',
-              //       description: S.of(context).defaultDescription,
-              //       title: 'university',
-              //       date: '2019 - 2020',
-              //     ),
-              //     TimeLineTile(
-              //       position: 'Graphic Design',
-              //       description: S.of(context).defaultDescription,
-              //       title: 'university',
-              //       date: '2019',
-              //     ),
-              //     TimeLineTile(
-              //       position: 'Graphic Design',
-              //       description: S.of(context).defaultDescription,
-              //       title: 'university',
-              //       date: '2020',
-              //     ),
-              //   ],
-              // ),
-              // SliverToBoxAdapter(
-              //   child: TextHint(text: 'Experience'),
-              // ),
-              // SliverStaggeredGrid.extent(
-              //   key: ValueKey('Experience'),
-              //   maxCrossAxisExtent: 600,
-              //   staggeredTiles: [
-              //     StaggeredTile.fit(1),
-              //     StaggeredTile.fit(1),
-              //     StaggeredTile.fit(1),
-              //   ],
-              //   children: [
-              //     TimeLineTile(
-              //       position: 'Graphic Design',
-              //       description: S.of(context).defaultDescription,
-              //       title: 'university',
-              //       date: '2019 - 2020',
-              //     ),
-              //     TimeLineTile(
-              //       position: 'Graphic Design',
-              //       description: S.of(context).defaultDescription,
-              //       title: 'university',
-              //       date: '2019',
-              //     ),
-              //     TimeLineTile(
-              //       position: 'Graphic Design',
-              //       description: S.of(context).defaultDescription,
-              //       title: 'university',
-              //       date: '2020',
-              //     ),
-              //   ],
-              // ),
-              // SliverToBoxAdapter(
-              //   child: TextHint(text: 'Skills'),
-              // ),
-            ],
-          ),
+            ),
+            SliverToBoxAdapter(
+              child: TextHint(text: 'What I Do'),
+            ),
+            SliverToBoxAdapter(
+              child: WhatIDoWidget(),
+            ),
+            // SliverToBoxAdapter(child: TextHint(text: 'Education')),
+            // SliverStaggeredGrid.extent(
+            //   maxCrossAxisExtent: 600,
+            //   staggeredTiles: [
+            //     StaggeredTile.fit(1),
+            //     StaggeredTile.fit(1),
+            //     StaggeredTile.fit(1),
+            //   ],
+            //   children: [
+            //     TimeLineTile(
+            //       position: 'Graphic Design',
+            //       description: S.of(context).defaultDescription,
+            //       title: 'university',
+            //       date: '2019 - 2020',
+            //     ),
+            //     TimeLineTile(
+            //       position: 'Graphic Design',
+            //       description: S.of(context).defaultDescription,
+            //       title: 'university',
+            //       date: '2019',
+            //     ),
+            //     TimeLineTile(
+            //       position: 'Graphic Design',
+            //       description: S.of(context).defaultDescription,
+            //       title: 'university',
+            //       date: '2020',
+            //     ),
+            //   ],
+            // ),
+            // SliverToBoxAdapter(
+            //   child: TextHint(text: 'Experience'),
+            // ),
+            // SliverStaggeredGrid.extent(
+            //   key: ValueKey('Experience'),
+            //   maxCrossAxisExtent: 600,
+            //   staggeredTiles: [
+            //     StaggeredTile.fit(1),
+            //     StaggeredTile.fit(1),
+            //     StaggeredTile.fit(1),
+            //   ],
+            //   children: [
+            //     TimeLineTile(
+            //       position: 'Graphic Design',
+            //       description: S.of(context).defaultDescription,
+            //       title: 'university',
+            //       date: '2019 - 2020',
+            //     ),
+            //     TimeLineTile(
+            //       position: 'Graphic Design',
+            //       description: S.of(context).defaultDescription,
+            //       title: 'university',
+            //       date: '2019',
+            //     ),
+            //     TimeLineTile(
+            //       position: 'Graphic Design',
+            //       description: S.of(context).defaultDescription,
+            //       title: 'university',
+            //       date: '2020',
+            //     ),
+            //   ],
+            // ),
+            // SliverToBoxAdapter(
+            //   child: TextHint(text: 'Skills'),
+            // ),
+          ],
         ),
       ),
     );
