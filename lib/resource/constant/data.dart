9f7adaf89ff8a209ccae7b9f9588fc52868da364
Payam Zahedi <pzahedi1995@gmail.com>
2021-06-12 13:01:41 +0430

    skills constants added

--- lib/resource/constant/data.dart
+++ lib/resource/constant/data.dart
@@ -1,1 +1,2 @@
-const defaultImageAvatarUrl = 'https://i.postimg.cc/7Z890j6V/20181019-210248.jpg';
+const defaultImageAvatarUrl =
+    'https://i.postimg.cc/7Z890j6V/20181019-210248.jpg';
---
+++ lib/resource/constant/skill.dart
@@ -0,0 +1,40 @@
+
+final softwareSkills = [
+  'Python & Django',
+  'Git',
+  'SOLID',
+  'OOP',
+  'TTD',
+  'CI/CD',
+  'Reactive Programing',
+  'Github Actions',
+  'MVC-MVP-MVVM',
+];
+
+final flutterSkills = [
+  'Flutter',
+  'Dart',
+  'BloC Pattern',
+  'Elements & RenderObjects',
+  'Dio & http',
+  'i18n & l10n',
+  'Plugging and Package',
+  'Streams & Futures',
+  'Isolate & EventLoop',
+  'RxDart',
+  'Animations',
+];
+
+final androidSkills = [
+  'Android SDK',
+  'JAVA',
+  'Kotlin',
+  'Retrofit & Volley',
+  'Animations & Transitions',
+];
+
+final designSkills = [
+  'Adobe XD',
+  'Material Design',
+  'Cupertino',
+];
