4925186667bf65b2e9a06d17af2dec5642624a14
Payam Zahedi <pzahedi1995@gmail.com>
2021-06-13 11:23:59 +0430

    skills images added

--- lib/resource/constant/data.dart
+++ lib/resource/constant/data.dart
@@ -1,2 +1,22 @@
-const defaultImageAvatarUrl =
-    'https://i.postimg.cc/7Z890j6V/20181019-210248.jpg';
+const defaultResourceUrl =
+    'https://raw.githubusercontent.com/payam-zahedi/payam-zahedi.github.io/main/resource';
+
+/// avatar image
+const defaultImageAvatarUrl = '$defaultResourceUrl/image/profile.jpg';
+
+/// cv file
+const cvUrl = '$defaultResourceUrl/cv/payamzahedi_cv.pdf';
+
+
+/// skills icons
+const adobeXd = '$defaultResourceUrl/icon/code/adobe-xd.png';
+const bloc = '$defaultResourceUrl/icon/code/bloc.png';
+const ci_cd = '$defaultResourceUrl/icon/code/ci-cd.png';
+const dart = '$defaultResourceUrl/icon/code/dart.png';
+const flutter = '$defaultResourceUrl/icon/code/flutter.png';
+const githubActions = '$defaultResourceUrl/icon/code/gh-actions.png';
+const git = '$defaultResourceUrl/icon/code/git.png';
+const java = '$defaultResourceUrl/icon/code/java.png';
+const kotlin = '$defaultResourceUrl/icon/code/kotlin.png';
+const python = '$defaultResourceUrl/icon/code/python.png';
+const reactive = '$defaultResourceUrl/icon/code/reactive.png';
