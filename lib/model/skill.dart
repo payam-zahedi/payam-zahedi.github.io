498ab78200952802eae076725defb539fbb5526c
Payam Zahedi <pzahedi1995@gmail.com>
2021-06-13 11:23:38 +0430

    SkillModel created

---
+++ lib/model/skill.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/painting.dart';
+
+class SkillModel {
+  const SkillModel({
+    required this.name,
+    this.imageUrl,
+    this.color,
+  });
+
+  final String name;
+  final String? imageUrl;
+  final Color? color;
+
+
+  bool get hasImage => imageUrl?.isNotEmpty ?? false;
+}
